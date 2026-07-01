422a76bc4acc15787a8c0f93753f570505df8255
1Boaz <bizbizdrori@gmail.com>
2026-07-01 21:50:12 +0300

    documented the code better + merged init package updated README.md + changed go.mod for easier installation

--- Init/Init.go
+++ Init/Init.go
@@ -1,14 +1,39 @@
 package Init
 
 import (
+	"flag"
+	"log"
 	"net"
 	"strconv"
 )
 
+// Init Main initializing function to call other functions in this package in sequence
 func Init() net.Listener {
-	port := Parse()
+	port := parse()
+	return createServer(strconv.Itoa(port))
+}
 
-	listener := CreateServer(strconv.Itoa(port))
+// createServer Initializes a tcp server and return the listener(server)
+func createServer(port string) net.Listener {
+	listener, err := net.Listen("tcp", ":"+port)
+	// verifies that net.Listen executed without errors
+	if err != nil {
+		log.Fatal("Couldn't use the selected port(" + port + "), Error: " + err.Error())
+	}
 
 	return listener
+}
+
+// parse parses the port cmd argument
+func parse() int {
+	// crates a flag named port, with a default value and a help message
+	var Port = flag.Int("port", 31415, "port to listen and send on")
+	flag.Parse()
+
+	// verifies flag(port) number is valid
+	if *Port > 65_535 || *Port < 1 {
+		log.Fatal("Port must be lower than 65_535(max port number) and higher than 1")
+	}
+
+	return *Port
 }
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "peer-chat/Init"
+import (
+	"log"
+	"net"
 
+	"github.com/1Boaz/peer-chat/Init"
+)
+
+// Main function should be minimal
 func main() {
-	_ = Init.Init()
+	server := Init.Init()
+
+	// closes the connection when program is done
+	defer func(server net.Listener) {
+		err := server.Close()
+		if err != nil {
+			log.Fatal("Error closing server: " + err.Error())
+		}
+	}(server)
 }
